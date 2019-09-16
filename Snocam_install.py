3b930c978d84339c6cc9033bd9906cdc4fdc5c57
Jackson Sugar <33731112+jacksonsugar@users.noreply.github.com>
2019-09-16 11:50:42 -0400

    Update Snocam_install.py

--- Snocam_install.py
+++ Snocam_install.py
@@ -116,25 +116,8 @@ os.system('sudo cp -r /tsys01-python/tsys01 /home/pi/Documents/Minion_scripts/')
 os.chdir(ini_dir)
 
 # Set up and sync RTC
-RTC_type = input('What RTC are you using? \n --- (1): pcf8523 \n --- (2): DS3231\n --- ')
-
-if len(str(RTC_type)) != 1 or RTC_type < 1 or RTC_type > 2:
-	RTC_fail = 1
-	while RTC_fail == 1: 
-		RTC_addr = input('Please select RTC using the numbers below: \n --- (1): pcf8523 \n --- (2): DS3231 \n --- ')
-		if len(str(RTC_type)) != 1 or RTC_type < 1 or RTC_type > 2:
-			pass
-		else:
-			RTC_fail = 0
-else:
-	print "Appending /boot/config.txt"
-
-if RTC_type == 1:
-	os.system("echo 'dtoverlay=i2c-rtc,pcf8523' >> /boot/config.txt")
-elif RTC_type == 2:
-	os.system("echo 'dtoverlay=i2c-rtc,ds3231' >> /boot/config.txt")
-else:
-	print "How did you get here?"
+print "Appending /boot/config.txt"
+os.system("echo 'dtoverlay=i2c-rtc,ds3231' >> /boot/config.txt")
 
 # Set pi to launch rest of script after reboot
 os.system("sudo sed -i '/# Print the IP/isudo python /home/pi/Documents/Minion_scripts/RTC_Finish.py\n\n' /etc/rc.local")
